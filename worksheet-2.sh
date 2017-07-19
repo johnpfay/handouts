e3b1123fd451861c79c5d167d7ce46860e54caf8
johnpfay <john.fay@duke.edu>
2017-07-19 13:30:03 +0000

    End of day 1

--- worksheet-2.sh
+++ worksheet-2.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 ## Configure git
-git config --global user.name ...
-git config --global user.email ...
+git config --global user.name johnpfay
+git config --global user.email john.fay@duke.edu
 
 ## Change the "origin" remote URL and push
-git remote set-url origin ...
+git remote set-url origin https://github.com/johnpfay/handouts.git
 
 ## Set the SESYNC-CI repository upstream and pull updates
 git remote add upstream https://github.com/sesync-ci/handouts.git
--- worksheet-4.R
+++ worksheet-4.R
@@ -2,66 +2,84 @@
 
 counts_df <- data.frame(
   day = c("Monday", "Tuesday", "Wednesday"),
-  wolf = c(2, ...),
-  hare = ...,
-  ...
+  wolf = c(2, 1, 3),
+  hare = c(20, 25, 30),
+  fox = c(4, 4, 4)
 )
 
 ## Reshaping multiple columns in category/value pairs
 
 library(tidyr)
 counts_gather <- gather(counts_df,
-                        ...,
-                        ...,
-                        ...)
+                        key = 'species',
+                        value ='count',
+                        wolf:fox)
 
 counts_spread <- spread(counts_gather,
-                        ...,
-                        ...)
+                        key = species,
+                        value = count,
+                        na.rm = TRUE)
 
 ## Exercise 1
 
-...
+#Remove record
+df1 = counts_gather[-8,]
+#Spread
+df_spr = spread(df1,
+                key=species, 
+                value=count,
+                fill = 0)
+df_spr1 = spread(df1,
+                key=species, 
+                value=count)
+df_spr1 <- drop_na(df_spr1)
 
 ## Read comma-separated-value (CSV) files
 
 animals <- ...
 
-animals <- read.csv('data/animals.csv', )
+animals <- read.csv('data/animals.csv', na.strings='')
 
 library(dplyr)
-library(...)
+library(RPostgreSQL)
 
-con <- ...(..., host = 'localhost', dbname = 'portal')
-animals_db <- ...
-animals <- ...
-dbDisconnect(...)
+con <- dbConnect(PostgreSQL(), host = 'localhost', dbname = 'portal')
+animals_db <- tbl(con, 'animals')
+#Copies all records to local environment
+animals <- collect(animals_db)
+#Close the connection
+dbDisconnect(con)
 
 ## Subsetting and sorting
 
 library(dplyr)
-animals_1990_winter <- filter(...,
-                              ...,
-                              ...)
+#Nab winter 1990 records
+animals_1990_winter <- filter(animals,
+                              year == 1990,
+                              month %in% 1:3)
+#Drop year column
+animals_1990_winter <- select(animals_1990_winter, -year)
 
-animals_1990_winter <- select(animals_1990_winter, ...)
-
-sorted <- ...(animals_1990_winter,
-              ...)
-
+sorted <- arrange(animals_1990_winter,
+              desc(species_id), weight)
 ## Exercise 2
 
 ...
 
 ## Grouping and aggregation
 
-animals_1990_winter_gb <- group_by(...)
+animals_1990_winter_gb <- group_by(animals_1990_winter, species_id)
 
-counts_1990_winter <- summarize(..., count = n())
+counts_1990_winter <- summarize(animals_1990_winter_gb, count = n())
 
 ## Exercise 3
-
-...
+#Select animal = DM
+dmAll = filter(animals,species_id == 'DM')
+#Group by month
+dmGroup = group_by(dmAll,month)
+#Summarize
+dmSummary = summarize(dmGroup, avg_wt=mean(weight, na.rm=TRUE), avg_hf=mean(hindfoot_length,na.rm=TRUE))
+View(dmSummary)
 
 ## Pivot tables through aggregate and spread
 
@@ -71,9 +89,8 @@ pivot <- ...
 
 ## Transformation of variables
 
-prop_1990_winter <- mutate(...,
-                           ...)
-
+prop_1990_winter <- mutate(counts_1990_winter,
+                           prop = count/sum(count))
 ## Exercise 4
 
 ...
@@ -81,8 +98,9 @@ prop_1990_winter <- mutate(...,
 ## Chainning with pipes
 
 prop_1990_winter_piped <- animals %>%
-  filter(year == 1990, month %in% 1:3)
-  ... # select all columns but year
-  ... # group by species_id
-  ... # summarize with counts
-  ... # mutate into proportions
+  filter(year == 1990, month %in% 1:3) %>%
+  select(-year) %>% 
+  group_by(species_id) %>% 
+  summarize(count = n()) %>%
+  mutate(prop = count/sum()) 
+
